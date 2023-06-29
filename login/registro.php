075198e8231394e1b30a0c366ea434571e910388
Juan Acosta <angelmex121@gmail.com>
2023-06-29 09:33:56 -0400

    feat: :art: boton hecho

--- login/registro.php
+++ login/registro.php
@@ -7,6 +7,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Login /Software Libre Blog</title>
+  <script>
+    function mostrarAlerta() {
+      alert("Usuario registrado con éxito");
+    }
+  </script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
 </head>
 
@@ -39,7 +44,7 @@
         <label for="exampleInputPassword1" class="form-label blanco"></label>
         <input type="password" class="form-control bg-light " name="confirm-password" required placeholder="Confirmar Contraseña">
       </div>
-      <button type="submit" class="btn btn-info text-white w-100 mt-4 fw-semibold shadow-sm" value="entrar" name="entrar">
+      <button type="submit" onclick="mostrarAlerta()" class="btn btn-info text-white w-100 mt-4 fw-semibold shadow-sm" value="entrar" name="entrar">
         Registrarse
       </button>
       <div class="p-3">
